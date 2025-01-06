dc471263bf26dcf3fe679351a87115d109aae9fa
Victor Elgersma <victor.elgersma41@thg.com>
2025-01-06 22:29:53 +0000

    wip: did part 2

--- part-1/script.py
+++ part-1/script.py
@@ -1,6 +1,5 @@
 #!/opt/homebrew/bin/python3
 
-
 def f1(an_int):
     """
     >>> f1(1)
@@ -13,12 +12,27 @@ def f1(an_int):
         n = n+1
     return n+1
 
+def f2(an_int):
+    """
+    >>> f2(234)
+    '00:03:54'
+    >>> f2(7322)
+    '02:02:02'
+    """
+    hours = 0
+    minutes = 0
+    seconds = an_int
+    minutes = seconds // 60
+    hours = seconds // 3600
+    if (minutes):
+        seconds = (seconds - minutes*60)
+        if (hours):
+            minutes = (minutes-hours*60)
+    
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
-def f2(an_int):
-    """
-    >>>
 
-    """
