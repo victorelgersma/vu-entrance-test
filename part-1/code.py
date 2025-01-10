854388f0985c637706c6744789db2366f14808ff
Victor Elgersma <victor.elgersma41@thg.com>
2025-01-10 18:52:00 +0000

    fixed first question

--- part-1/code.py
+++ part-1/code.py
@@ -2,18 +2,20 @@
 
 def f1(an_int):
     """
+    Return the greatest power of 3 that is smaller than or equal to an_int
     >>> f1(1)
     1
     >>> f1(10)
     9
     """
     n=0
-    while (3^n<=an_int):
-        n = n+1
-    return n+1
+    while (3 ** n<=an_int):
+        n += 1
+    return 3 ** (n-1)
 
 def f2(an_int):
     """
+    The function should return a string that contains the time in the format hh:mm:ss.
     >>> f2(234)
     '00:03:54'
     >>> f2(7322)
@@ -31,8 +33,18 @@ def f2(an_int):
     
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
+
+# def f3(a_list):
+#     """
+#     >>> f3([4,5,6])
+#     '(1,5)'
+#     >>> f3([-3,-5,7])
+#     '(0,-3)'
+#     """
+    
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-
 
--- readme.md
+++ readme.md
@@ -2,5 +2,8 @@
 
 Programming assignment / entry test for course AI at the VU
 
+## Part 1
+
 https://phoenix.labs.vu.nl/assessment/files/assignment_one.pdf
+
 # vu-entrance-test
